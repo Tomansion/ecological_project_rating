d8eb5dbafeee0089d29ad71228415be1c849538e
tom.mansion <tomansion@yahoo.fr>
2021-12-08 09:52:59 +0100

    layout OK

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="section">
+
+    </div>
+    <div class="section">
+
+    </div>
+    <div class="section">
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: space-between;
+  width: 100vw;
+  height: 100vh;
+  background-color: cadetblue;
+}
+.section {
+  border: solid 1px #ccc;
+  flex: 1;
+}
+
+/* Change the sections directions when screen is on portrait mode */
+@media screen and (orientation: portrait) {
+  #app {
+    flex-direction: column;
+  }
 }
 </style>
