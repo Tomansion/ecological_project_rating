8726a1c87f040623bff70e81f371167feea622e7
tom.mansion <tomansion@yahoo.fr>
2021-12-08 12:06:37 +0100

    style

--- src/components/RadarChart.vue
+++ src/components/RadarChart.vue
@@ -13,7 +13,7 @@ export default {
     },
   },
   mounted() {
-      this.updateRadar();
+    this.updateRadar();
   },
   methods: {
     updateRadar() {
@@ -27,22 +27,30 @@ export default {
       let data = [
         {
           type: "scatterpolar",
-
           r: this.criteriaValues.map(
             (criteria) => criteria.value * criteria.coef
           ),
-
           theta: this.criteriaValues.map((criteria) => criteria.name),
-
           fill: "toself",
         },
       ];
+
+      // Add value to the end to loop the radar
+      if (this.criteriaValues.length > 0) {
+        data[0].r.push(this.criteriaValues[0].value * this.criteriaValues[0].coef);
+        data[0].theta.push(this.criteriaValues[0].name);
+      }
 
       let layout = {
         polar: {
           radialaxis: {
             visible: true,
             range: [0, max],
+            colcolor: "gray",
+            linewidth: 0,
+            tickmode: "linear",
+            tick0: 0,
+            dtick: max/2 ,
           },
         },
 
